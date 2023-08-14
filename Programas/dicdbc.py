04e487ae656d0678c83bb30f7253609e7a63c772
Arthur breno <arthursrsbreno@gmail.com>
2023-08-14 01:08:27 -0300

    Update dicdbc.py

--- Programas/dicdbc.py
+++ Programas/dicdbc.py
@@ -1,39 +1,110 @@
-#https://www.pythonfordatascience.org/anova-python/
-#https://doex.rohitsanjay.com/en/latest/examples.html#randomized-complete-block-design
-
+mport pandas as pd
+import numpy as np
+from scipy.stats import f_oneway
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 import doex
-import pandas as pd
-import scipy.stats as stats
-import researchpy as rp
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-# oq caraio é df = dataFrame
-#DIC
-# df_dic = pd.read_csv('exemplo_dic.txt')
 
-# def anova_table(aov):
-#      aov['mean_sq'] = aov[:]['sum_sq']/aov[:]['df']
-#
-#      aov['eta_sq'] = aov[:-1]['sum_sq']/sum(aov['sum_sq'])
-#
-#      aov['omega_sq'] = (aov[:-1]['sum_sq']-(aov[:-1]['df']*aov['mean_sq'][-1]))/(sum(aov['sum_sq'])+aov['mean_sq'][-1])
-#
-#      cols = ['sum_sq', 'df', 'mean_sq', 'F', 'PR(>F)', 'eta_sq', 'omega_sq']
-#      aov = aov[cols]
-#      return aov
-#
-#
-# model = ols('Peso ~ Racao', data=df_dic).fit()
-# aov_table = sm.stats.anova_lm(model, typ=2)
-# print(anova_table(aov_table))
-# ---------------------------------------------------------------------------------------------------------------------------
-#DBC
-# exp = doex.RandomizedCompleteBlockDesign(
-#     [
-#         [60,50,65,40],
-#         [70,80,75,90],
-#         [100,90,85,75],
-#         [90,110,95,105],
-#         [120,105,140,95],
-#     ]
-# )
+#----------DIC--------------------------------------------------------
+data = pd.read_csv("exemplo_dic.csv")
+print(data.size)
+#construir lista
+#definir numero de repetições...
+repeticoes = 1
+for i in range(0, data.size):
+    if data['Racao'][i + 1] == data['Racao'][i]:
+        repeticoes = repeticoes + 1
+    else:
+        break
+print(repeticoes)
+#numero de repetições deifnido
+numeroTratamentos = int((data.size/2)/repeticoes)
+lista = []
+for i in range(0, numeroTratamentos):
+    lista.append([])
+for i in range(0, numeroTratamentos):
+    for ii in range(0, repeticoes):
+        lista[i].append(0)
+iteradorRepecoes = 0
+iteradorTratamentos = 0
+contador = 0
+print(lista)
+while True:
+    print("ITERA", contador)
+    lista[iteradorTratamentos][iteradorRepecoes] = data['Peso'][contador]
+    iteradorRepecoes = iteradorRepecoes + 1
+    if contador > 1 and iteradorRepecoes == repeticoes:
+        iteradorTratamentos = iteradorTratamentos + 1
+        iteradorRepecoes = 0
+    if contador > ((data.size/2)-2):
+        break
+    else:
+        contador = contador + 1
+#--------LISTA CONSTRUIDA---------------
+exp = doex.CompletelyRandomizedDesign(lista[0], lista[1], lista[2], lista[3])
+
+#---------------------------------------------------------------------------------------------
+segundaLista = []
+for i in range(0, int(data.size/2)):
+    segundaLista.append(data['Peso'][i])
+print(segundaLista)
+print(len(segundaLista))
+df = pd.DataFrame({'Peso': segundaLista, 'Racao': np.repeat(['a', 'b', 'c', 'd'], repeats=6)})
+
+tukey = pairwise_tukeyhsd(endog=df['Peso'],
+                          groups=df['Racao'],
+                          alpha=0.05)
+print(tukey)
+
+#---------------------------DBC---------------------------------------------------------------------------------
+data = pd.read_csv("exemplo_dbc.csv")
+print(data.size)
+
+#construir lista
+#definir numero de repetições...
+repeticoes = 1
+for i in range(0, data.size):
+    if data['tratamentos'][i + 1] == data['tratamentos'][i]:
+        repeticoes = repeticoes + 1
+    else:
+        break
+print(repeticoes)
+#numero de repetições deifnido
+numeroTratamentos = int((data.size/3)/repeticoes)
+lista = []
+for i in range(0, numeroTratamentos):
+    lista.append([])
+for i in range(0, numeroTratamentos):
+    for ii in range(0, repeticoes):
+        lista[i].append(0)
+iteradorRepecoes = 0
+iteradorTratamentos = 0
+contador = 0
+print(lista)
+while True:
+    print("ITERA", contador)
+    lista[iteradorTratamentos][iteradorRepecoes] = data['resultados'][contador]
+    iteradorRepecoes = iteradorRepecoes + 1
+    if contador > 1 and iteradorRepecoes == repeticoes:
+        iteradorTratamentos = iteradorTratamentos + 1
+        iteradorRepecoes = 0
+        print("BUCETAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    if contador > ((data.size/3)-2):
+        break
+    else:
+        contador = contador + 1
+#--------LISTA CONSTRUIDA---------------
+
+exp = doex.RandomizedCompleteBlockDesign(lista)
+
+#---------------------------------------------------------------------------------------------
+segundaLista = []
+for i in range(0, int(data.size/3)):
+    segundaLista.append(data['resultados'][i])
+print(segundaLista)
+print(len(segundaLista))
+df = pd.DataFrame({'resultados': segundaLista, 'tratamentos': np.repeat(['a', 'b', 'c', 'd', 'e'], repeats=4)})
+
+tukey = pairwise_tukeyhsd(endog=df['resultados'],
+                          groups=df['tratamentos'],
+                          alpha=0.05)
+print(tukey)
